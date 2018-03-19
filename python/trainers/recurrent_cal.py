7e6b06e5576bd2dc107e977411e0f8dfff605be4
Shah Rukh Qasim <ishahrukhqasim@gmail.com>
2018-03-19 14:44:39 +0100

    feat: Add support for custom training format

--- python/trainers/recurrent_cal.py
+++ python/trainers/recurrent_cal.py
@@ -3,6 +3,7 @@ import configparser as cp
 from ..models.recurrent_cal import RecurrentCal
 import numpy as np
 import os
+from DeepJetCore.DataCollection import DataCollection
 
 
 class RecurrentCalTrainer:
@@ -21,6 +22,8 @@ class RecurrentCalTrainer:
         self.summary_path = self.config['summary_path']
         self.train_for_iterations = int(self.config['train_for_iterations'])
         self.save_after_iterations = int(self.config['save_after_iterations'])
+        self.use_tf_records = int(self.config['use_tf_records'])==1
+        self.batch_size = int(self.config['batch_size'])
 
     def _bytes_feature(value):
         return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -35,17 +38,10 @@ class RecurrentCalTrainer:
             except Exception as e:
                 print(e)
 
-    def train(self):
+    def get_record_placeholders(self):
         with open(self.config['training_files_list']) as f:
             content = f.readlines()
         file_paths = [x.strip() for x in content]
-
-        placeholder_input, placeholder_output = self.model.get_placeholders()
-        graph_output = self.model.get_compute_graphs()
-        graph_loss = self.model.get_losses()
-        graph_optmiser = self.model.get_optimizer()
-        graph_summary = self.model.get_summary()
-
         options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.GZIP)
         reader = tf.TFRecordReader(options=options)
         filename_queue = tf.train.string_input_producer(file_paths)
@@ -61,14 +57,39 @@ class RecurrentCalTrainer:
             [record_input, record_target], batch_size=int(self.config['batch_size']), capacity=int(self.config['batch_size'])*10,
             min_after_dequeue=int(self.config['batch_size']))
 
+        return record_batch_input, record_batch_target
+
+
+    def train(self):
+
+        placeholder_input, placeholder_output = self.model.get_placeholders()
+        graph_output = self.model.get_compute_graphs()
+        graph_loss = self.model.get_losses()
+        graph_optmiser = self.model.get_optimizer()
+        graph_summary = self.model.get_summary()
+
+
         if self.from_scratch:
             self.clean_summary_dir()
 
         init = tf.global_variables_initializer()
         with tf.Session() as sess:
             sess.run(init)
-            coord = tf.train.Coordinator()
-            threads = tf.train.start_queue_runners(coord=coord)
+            if self.use_tf_records:
+                coord = tf.train.Coordinator()
+                threads = tf.train.start_queue_runners(coord=coord)
+            else:
+                input_data = "/home/srq2/Datasets/three_hgcal/out/dataCollection.dc"
+                train_data = DataCollection()
+                train_data.readFromFile(input_data)
+
+                val_data = train_data.split(0.1)
+                train_data = train_data.split(0.9)
+                train_data.setBatchSize(self.batch_size)
+                val_data.setBatchSize(self.batch_size)
+                val_data_generator = train_data.generator()
+                train_data_generator = train_data.generator()
+
             summary_writer = tf.summary.FileWriter(self.summary_path, sess.graph)
 
             if not self.from_scratch:
@@ -79,12 +100,19 @@ class RecurrentCalTrainer:
             else:
                 iteration_number = 0
 
+            record_batch_input, record_batch_target = self.get_record_placeholders()
+
             print("Starting iterations")
             while iteration_number < self.train_for_iterations:
-                input, output = sess.run([record_batch_input, record_batch_target])
-                input = [np.fromstring(''.join(i)).reshape(13, 13, int(self.config['num_layers']),
-                                                           int(self.config['num_channels'])) for i in input]
-                output = [np.fromstring(''.join(i)).reshape(13, 13, int(self.config['num_layers'])) for i in output]
+                if self.use_tf_records:
+                    input, output = sess.run([record_batch_input, record_batch_target])
+                    input = [np.fromstring(''.join(i)).reshape(13, 13, int(self.config['num_layers']),
+                                                               int(self.config['num_channels'])) for i in input]
+                    output = [np.fromstring(''.join(i)).reshape(13, 13, int(self.config['num_layers'])) for i in output]
+                else:
+                    input, output,_ = train_data_generator.next()
+                    input = np.squeeze(input,axis=0)
+                    output = np.squeeze(output,axis=0)
 
                 _, eval_loss, _ , eval_summary= sess.run([graph_output, graph_loss, graph_optmiser, graph_summary],
                                       feed_dict={placeholder_input: input, placeholder_output: output})
@@ -96,9 +124,9 @@ class RecurrentCalTrainer:
                     self.saver_all.save(sess, self.model_path)
                     with open(self.model_path + '.txt', 'w') as f:
                         f.write(str(iteration_number))
+            if self.use_tf_records:
+                # Stop the threads
+                coord.request_stop()
 
-            # Stop the threads
-            coord.request_stop()
-
-            # Wait for threads to stop
-            coord.join(threads)
+                # Wait for threads to stop
+                coord.join(threads)
