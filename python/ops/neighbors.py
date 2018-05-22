dcdb163acb3c69acf9eb53a0a9d36e25a7b36549
Shah Rukh Qasim <ishahrukhqasim@gmail.com>
2018-05-22 23:13:44 +0200

    feat: Add ops for finding nearest neighbors, pooling

---
+++ python/ops/neighbors.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+
+
+def euclidean_two_batches(A, B):
+    """
+    Returns euclidean distance between two batches of shape [B,N,F] and [B,M,F] where B is batch size, N is number of
+    examples in the batch of first set, M is number of examples in the batch of second set, F is number of spatial
+    features.
+
+    Returns:
+    A matrix of size [B, N, M] where each element [i,j] denotes euclidean distance between ith entry in first set and
+    jth in second set.
+
+    """
+
+    shape_A = A.get_shape().as_list()
+    shape_B = B.get_shape().as_list()
+
+    assert (A.dtype == tf.float32 or A.dtype == tf.float64) and (B.dtype == tf.float32 or B.dtype == tf.float64)
+    assert len(shape_A) == 3 and len(shape_B) == 3
+    assert shape_A[0] == shape_B[0] and shape_A[1] == shape_B[1]
+
+    # Finds euclidean distance using property (a-b)^2 = a^2 + b^2 - 2ab
+    sub_factor = -2 * tf.matmul(A, tf.transpose(B, perm=[0, 2, 1]))         # -2ab term
+    dotA = tf.expand_dims(tf.reduce_sum(A * A, axis=2), axis=2)             # a^2 term
+    dotB = tf.expand_dims(tf.reduce_sum(B * B, axis=2), axis=1)             # b^2 term
+
+    return sub_factor + dotA + dotB                                         # Euclidean distance
+
+
+def nearest_neighbor_matrix(spatial_features, k=10):
+    """
+    Nearest neighbors matrix given spatial features.
+
+    :param spatial_features: Spatial features of shape [B, N, S] where B = batch size, N = max examples in batch,
+                             S = spatial features
+    :param k: Max neighbors
+    :return:
+    """
+
+    shape = spatial_features.get_shape().as_list()
+
+    assert spatial_features.dtype == tf.float32 or spatial_features.dtype == tf.float64
+    assert len(shape) == 3
+
+    D = euclidean_two_batches(spatial_features, spatial_features)
+    _, N = tf.nn.top_k(-D, k)
+    return N
+
+
+def indexing_tensor(spatial_features, k=10):
+
+    shape_spatial_features = spatial_features.get_shape().as_list()
+
+    n_batch = shape_spatial_features[0]
+    n_max_entries = shape_spatial_features[1]
+
+    # All of these tensors should be 3-dimensional
+    assert len(shape_spatial_features) == 3
+
+    # Neighbor matrix should be int as it should be used for indexing
+    assert spatial_features.dtype == tf.float64 or spatial_features.dtype == tf.float32
+
+    neighbor_matrix = nearest_neighbor_matrix(spatial_features, k)
+
+    batch_range_vector = tf.expand_dims(tf.expand_dims(tf.expand_dims(tf.range(0, n_batch), axis=1),axis=1), axis=1)
+    batch_range_vector = tf.tile(batch_range_vector, [1,n_max_entries,k,1])
+    expanded_neighbor_matrix = tf.expand_dims(neighbor_matrix, axis=3)
+    _indexing_tensor = tf.concat([batch_range_vector, expanded_neighbor_matrix], axis=3)
+
+    return tf.cast(_indexing_tensor, tf.int64)
--- python/ops/sparse_conv.py
+++ python/ops/sparse_conv.py
@@ -19,117 +19,10 @@ def sparse_conv_delta(A, B):
     return A - tf.expand_dims(B, axis=2)
 
 
-def sparse_conv(space_features, all_features, neighbor_matrix, output_all=15):
-    """
-    Defines sparse convolution layer
-
-    :param space_features: Space like features. Should be of shape [batch_size, num_entries, num_features]
-    :param all_features: All features.  Should be of shape [batch_size, num_entries, num_features]
-    :param neighbor_matrix: Matrix which contains nearest neighbors of each entry in a row. Should be of shape
-                             [batch_size, num_entries, max_neighbors]
-
-    :return: Computation graph which defines the sparse convolution
-    """
-    shape_space_features = space_features.get_shape().as_list()
-    shape_all_features = all_features.get_shape().as_list()
-    shape_neighbor_matrix = neighbor_matrix.get_shape().as_list()
-
-    n_batch = shape_space_features[0]
-    n_max_entries = shape_space_features[1]
-    n_max_neighbors = shape_neighbor_matrix[2]
-    n_features_input_all = shape_all_features[2]
-    n_features_input_space = shape_space_features[2]
-
-    # All of these tensors should be 3-dimensional
-    assert len(shape_space_features) == 3 and len(shape_all_features) == 3 and len(shape_neighbor_matrix) == 3
-
-    # First dimension is batch, second is number of entries, hence these two should be same for all
-    assert shape_space_features[0] == shape_all_features[0] == shape_neighbor_matrix[0]
-    assert shape_space_features[1] == shape_all_features[1] == shape_neighbor_matrix[1]
-
-    # Neighbor matrix should be int as it should be used for indexing
-    assert neighbor_matrix.dtype == tf.int32
-
-    batch_range_vector = tf.expand_dims(tf.expand_dims(tf.expand_dims(tf.range(0, n_batch), axis=1),axis=1), axis=1)
-    batch_range_vector = tf.tile(batch_range_vector, [1,n_max_entries,n_max_neighbors,1])
-    expanded_neighbor_matrix = tf.expand_dims(neighbor_matrix, axis=3)
-    indexing_tensor = tf.concat([batch_range_vector, expanded_neighbor_matrix], axis=3)
-
-    gathered_space_1 = tf.gather_nd(space_features, indexing_tensor) # [B,E,S] -> [B,E,5,S]
-    # sparse_conv_delta(gathered_space_1, space_features) #  [B,E,5,S] -> [B,E,5,S]
-    delta_gathered_space_2 = tf.reshape(sparse_conv_delta(gathered_space_1, space_features), [n_batch, n_max_entries, -1])
-    delta_space_flattened = tf.layers.dense(inputs=delta_gathered_space_2, units=n_features_input_all, activation=tf.nn.relu)
-
-    dotted_space = tf.multiply(all_features, delta_space_flattened)
-
-    concatenated_all_features = tf.concat((dotted_space, space_features), axis=2)
-
-    all_pen_output = tf.layers.dense(inputs=concatenated_all_features, units=output_all, activation=tf.nn.relu)
-
-    all_output = tf.layers.dense(
-        tf.reshape(tf.gather_nd(all_pen_output, indexing_tensor), [n_batch, n_max_entries, -1]), units=output_all,
-        activation=tf.nn.relu)
-
-    gathered_space = tf.gather_nd(space_features, indexing_tensor)
-    gathered_space_2 = sparse_conv_delta(gathered_space, space_features)
-
-    multiplication_factor_space = tf.layers.dense(
-        tf.reshape(tf.gather_nd(all_pen_output, indexing_tensor), [n_batch, n_max_entries, -1]), units=n_features_input_space,
-        activation=tf.nn.relu)
-
-    add_to_space = tf.multiply(tf.layers.dense(tf.reshape(gathered_space_2, [n_batch, n_max_entries, -1]), units=n_features_input_space,
-        activation=tf.nn.relu), multiplication_factor_space)
-    space_output = add_to_space + space_features
-
-    return space_output, all_output
-
-
 # TODO: space_features is not used
 def sparse_conv_bare(space_features, all_features, neighbor_matrix, output_all=15):
     """
     Define very simple sparse convolution layer which doesn't have complex space/other features dot products
-
-    :param space_features: Space like features. Should be of shape [batch_size, num_entries, num_features]
-    :param all_features: All features.  Should be of shape [batch_size, num_entries, num_features]
-    :param neighbor_matrix: Matrix which contains nearest neighbors of each entry in a row. Should be of shape
-                             [batch_size, num_entries, max_neighbors]
-
-    :return: Computation graph which defines the sparse convolution
-    """
-    shape_space_features = space_features.get_shape().as_list()
-    shape_all_features = all_features.get_shape().as_list()
-    shape_neighbor_matrix = neighbor_matrix.get_shape().as_list()
-
-    n_batch = shape_space_features[0]
-    n_max_entries = shape_space_features[1]
-    n_max_neighbors = shape_neighbor_matrix[2]
-    n_features_input_all = shape_all_features[2]
-    n_features_input_space = shape_space_features[2]
-
-    # All of these tensors should be 3-dimensional
-    assert len(shape_space_features) == 3 and len(shape_all_features) == 3 and len(shape_neighbor_matrix) == 3
-
-    # First dimension is batch, second is number of entries, hence these two should be same for all
-    assert shape_space_features[0] == shape_all_features[0] == shape_neighbor_matrix[0]
-    assert shape_space_features[1] == shape_all_features[1] == shape_neighbor_matrix[1]
-
-    # Neighbor matrix should be int as it should be used for indexing
-    assert neighbor_matrix.dtype == tf.int32
-
-    batch_range_vector = tf.expand_dims(tf.expand_dims(tf.expand_dims(tf.range(0, n_batch), axis=1),axis=1), axis=1)
-    batch_range_vector = tf.tile(batch_range_vector, [1,n_max_entries,n_max_neighbors,1])
-    expanded_neighbor_matrix = tf.expand_dims(neighbor_matrix, axis=3)
-    indexing_tensor = tf.concat([batch_range_vector, expanded_neighbor_matrix], axis=3)
-
-    gathered_all = tf.reshape(tf.gather_nd(all_features, indexing_tensor), [n_batch, n_max_entries, -1])  # [B,E,F] -> [B,E,5*F]
-
-    return tf.layers.dense(gathered_all, units=output_all, activation=tf.nn.relu)
-
-
-
-def sparse_conv_2(space_features, all_features, neighbor_matrix, output_all=15, weight_init_width=1e-4):
-    """
-    Defines sparse convolution layer
 
     :param space_features: Space like features. Should be of shape [batch_size, num_entries, num_features]
     :param all_features: All features.  Should be of shape [batch_size, num_entries, num_features]
@@ -163,6 +56,44 @@ def sparse_conv_2(space_features, all_features, neighbor_matrix, output_all=15, 
     expanded_neighbor_matrix = tf.expand_dims(neighbor_matrix, axis=3)
     indexing_tensor = tf.concat([batch_range_vector, expanded_neighbor_matrix], axis=3)
 
+    gathered_all = tf.reshape(tf.gather_nd(all_features, indexing_tensor), [n_batch, n_max_entries, -1])  # [B,E,F] -> [B,E,5*F]
+
+    return tf.layers.dense(gathered_all, units=output_all, activation=tf.nn.relu), space_features
+
+
+def sparse_conv_2(space_features, all_features, indexing_tensor, output_all=15, weight_init_width=1e-4):
+    """
+    Defines sparse convolution layer
+
+    :param space_features: Space like features. Should be of shape [batch_size, num_entries, num_features]
+    :param all_features: All features.  Should be of shape [batch_size, num_entries, num_features]
+    :param neighbor_matrix: Matrix which contains nearest neighbors of each entry in a row. Should be of shape
+                             [batch_size, num_entries, max_neighbors]
+
+    :return: Computation graph which defines the sparse convolution
+    """
+    shape_space_features = space_features.get_shape().as_list()
+    shape_all_features = all_features.get_shape().as_list()
+    shape_indexing_tensor = indexing_tensor.get_shape().as_list()
+
+    n_batch = shape_space_features[0]
+    n_max_entries = shape_space_features[1]
+    n_features_input_all = shape_all_features[2]
+    n_features_input_space = shape_space_features[2]
+    n_max_neighbors = shape_indexing_tensor[2]
+
+    # All of these tensors should be 3-dimensional
+    # TODO: Add assert for indexing_tensor shape
+    assert len(shape_space_features) == 3 and len(shape_all_features) == 3 and len(shape_indexing_tensor) == 4
+
+    # First dimension is batch, second is number of entries, hence these two should be same for all
+    assert shape_space_features[0] == shape_all_features[0]
+    assert shape_space_features[1] == shape_all_features[1]
+
+    # Neighbor matrix should be int as it should be used for indexing
+    assert indexing_tensor.dtype == tf.int64
+
+
     gathered_space_1 = tf.gather_nd(space_features, indexing_tensor) # [B,E,5,S]
     delta_space = sparse_conv_delta(gathered_space_1, space_features) # [B,E,5,S]
 
@@ -170,6 +101,7 @@ def sparse_conv_2(space_features, all_features, neighbor_matrix, output_all=15, 
     # this is a small correction
     weighting_factor_for_all_features = tf.layers.dense(inputs=weighting_factor_for_all_features, units=n_max_neighbors, 
                                                         activation=tf.nn.relu) # [B,E,N]
+    # weighting_factor_for_all_features = tf.clip_by_value(weighting_factor_for_all_features, 0, 1e4)
     weighting_factor_for_all_features = 1 + tf.expand_dims(weighting_factor_for_all_features, axis=3)  # [B,E,N] - N = neighbors
 
     
@@ -183,9 +115,33 @@ def sparse_conv_2(space_features, all_features, neighbor_matrix, output_all=15, 
                                                             activation=tf.nn.relu, 
                                                         kernel_initializer=tf.random_normal_initializer(mean=0., stddev=weight_init_width),
                                                         bias_initializer=tf.random_normal_initializer(mean=0., stddev=weight_init_width))
+    # weighting_factor_for_spatial_features = tf.clip_by_value(weighting_factor_for_all_features, 0, 1e4)
     weighting_factor_for_spatial_features = 1 + tf.expand_dims(weighting_factor_for_spatial_features, axis=3)
 
-    
     spatial_output = space_features + tf.reduce_mean(delta_space * weighting_factor_for_spatial_features, axis=2)
 
     return output, spatial_output
+
+
+def sparse_max_pool(pooling_features, num_entries_result, graphs):
+    shape_spatial_features = pooling_features.get_shape().as_list()
+
+    n_batch = shape_spatial_features[0]
+
+    # All of these tensors should be 3-dimensional
+    assert len(shape_spatial_features) == 3
+
+    # Neighbor matrix should be int as it should be used for indexing
+    assert pooling_features.dtype == tf.float64 or pooling_features.dtype == tf.float32
+
+    _, I = tf.nn.top_k(tf.reduce_sum(pooling_features, axis=2), num_entries_result)
+    I = tf.expand_dims(I, axis=2)
+
+
+    batch_range = tf.expand_dims(tf.expand_dims(tf.range(0, n_batch), axis=1), axis=1)
+    batch_range = tf.tile(batch_range, [1,num_entries_result, 1])
+    _indexing_tensor = tf.concat([batch_range, I], axis=2)
+
+    return [tf.gather_nd(i, _indexing_tensor) for i in graphs]
+
+
