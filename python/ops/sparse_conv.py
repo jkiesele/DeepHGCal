909daff39954f34e03c87a93825b475fe896a441
Shah Rukh Qasim <ishahrukhqasim@gmail.com>
2018-08-31 14:06:41 +0200

    feat: Add spatial transformation based op

--- python/ops/sparse_conv.py
+++ python/ops/sparse_conv.py
@@ -532,14 +532,13 @@ def noisy_eye_initializer():
     return _initializer
 
 
-def sparse_conv_make_neighbors(sparse_dict, num_neighbors=10, output_all=15, weight_init_width=1e-4, control_switches = None):
+def sparse_conv_make_neighbors(sparse_dict, num_neighbors=10, output_all=15, spatial_degree_non_linearity=1, transformed_spatial_features=10, propagrate_ahead=False):
     """
     Defines sparse convolutional layer
 
     :param sparse_dict: Dictionary containing input
     :param num_neighbors: An integer containing number of neighbors to pick + 1 (+1 is for yourself)
     :param output_all: Number of output features for color like outputs
-    :param weight_init_width: TODO: Fill this
     :return: Dictionary containing output which can be made input to the next layer
     """
 
@@ -572,23 +571,24 @@ def sparse_conv_make_neighbors(sparse_dict, num_neighbors=10, output_all=15, wei
     # Neighbor matrix should be int as it should be used for indexing
     assert _indexing_tensor.dtype == tf.int64
 
+    assert spatial_degree_non_linearity >= 1
+
     transformed_space_features = tf.concat([spatial_features_global], axis=2)
+
+    for i in range(spatial_degree_non_linearity - 1):
+        transformed_space_features = tf.layers.dense(transformed_space_features, transformed_spatial_features, activation=tf.nn.relu)
+
     transformed_space_features = tf.layers.dense(transformed_space_features, 10, activation=None, kernel_initializer=NoisyEyeInitializer)
     # transformed_space_features = tf.layers.dense(transformed_space_features, 10, activation=tf.nn.relu)
-    transformed_space_features = tf.cond(tf.equal(control_switches[0],1), lambda: transformed_space_features, lambda: gradient_off(transformed_space_features))
 
     _indexing_tensor, distance = indexing_tensor_2(transformed_space_features, num_neighbors)
 
-    gathered_all = tf.gather_nd(all_features, _indexing_tensor) * tf.nn.softmax(0.1 + tf.expand_dims(distance, axis=3)) # [B,E,5,F]
+    gathered_all = tf.gather_nd(all_features, _indexing_tensor) * tf.nn.softmax(tf.expand_dims(-distance, axis=3)) # [B,E,5,F]
 
     pre_output = tf.layers.dense(gathered_all, output_all, activation=tf.nn.relu)
     output = tf.layers.dense(tf.reshape(pre_output, [n_batch, n_max_entries, -1]), output_all, activation=tf.nn.relu)
-    output = tf.cond(tf.equal(control_switches[1],1), lambda: output, lambda: gradient_off(output))
 
-    # TODO: Check if you have to place gradient switch up or down!
-
-    return construct_sparse_io_dict(output, spatial_features_global, spatial_features_local, num_entries)
-
+    return construct_sparse_io_dict(output, transformed_space_features if propagrate_ahead else spatial_features_global, spatial_features_local, num_entries)
 
 
 
