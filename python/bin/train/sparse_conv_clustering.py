a86dfd4e58b76dd60310ac054d3f58b81cbdee2e
Shah Rukh Qasim <ishahrukhqasim@gmail.com>
2018-09-27 13:17:09 +0200

    feat: Add clustering train script

---
+++ python/bin/train/sparse_conv_clustering.py
@@ -0,0 +1,21 @@
+from trainers.sparse_conv_clustering import SparseConvClusteringTrainer
+import argparse
+from trainers.sparse_conv_multiple_optimizers import SparseConvTrainerMulti
+
+
+parser = argparse.ArgumentParser(description='Run training for graph based clustering')
+parser.add_argument('input', help="Path to config file")
+parser.add_argument('config', help="Config section within the config file")
+parser.add_argument('--test', default=False, help="Whether to run evaluation on test set")
+parser.add_argument('--profile', default=False, help="Whether to run evaluation on test set")
+args = parser.parse_args()
+
+
+trainer = SparseConvClusteringTrainer(args.input, args.config)
+
+if args.test:
+    trainer.test()
+elif args.profile:
+    trainer.profile()
+else:
+    trainer.train()
